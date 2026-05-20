129d6edcbe769044fafa024c86c87c953890cd9e
Evelina <evelinazolota55@gmail.com>
2026-05-20 16:38:18 +0300

    Мини исправления 3

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
@@ -1,26 +1,26 @@
 package ru.yandex.practicum.collector.config;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
 
 @Configuration
-@EnableConfigurationProperties(KafkaProperties.class)
 public class KafkaConfig {
+
     @Bean
-    public KafkaProducer<String, byte[]> kafkaProducer(KafkaProperties kafkaProperties) {
+    public Producer<String, byte[]> kafkaProducer() {
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
-        properties.put(ProducerConfig.ACKS_CONFIG, "1");
-        properties.put(ProducerConfig.LINGER_MS_CONFIG, "5");
+
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer",
+                ByteArraySerializer.class.getName());
+
         return new KafkaProducer<>(properties);
     }
 }
--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/CollectorGrpcController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/controller/CollectorGrpcController.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.collector.controller;
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
@@ -10,16 +12,43 @@ import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 @GrpcService
 public class CollectorGrpcController extends CollectorControllerGrpc.CollectorControllerImplBase {
 
-    @Override
-    public void collectSensorEvent(SensorEventProto request,
-                                   StreamObserver<Empty> responseObserver) {
-        responseObserver.onNext(Empty.getDefaultInstance());
-        responseObserver.onCompleted();
+    private static final String HUB_TOPIC = "telemetry.hubs.v1";
+    private static final String SENSOR_TOPIC = "telemetry.sensors.v1";
+
+    private final Producer<String, byte[]> kafkaProducer;
+
+    public CollectorGrpcController(Producer<String, byte[]> kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
     }
 
     @Override
     public void collectHubEvent(HubEventProto request,
                                 StreamObserver<Empty> responseObserver) {
+
+        kafkaProducer.send(
+                new ProducerRecord<>(
+                        HUB_TOPIC,
+                        request.getHubId(),
+                        request.toByteArray()
+                )
+        );
+
+        responseObserver.onNext(Empty.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void collectSensorEvent(SensorEventProto request,
+                                   StreamObserver<Empty> responseObserver) {
+
+        kafkaProducer.send(
+                new ProducerRecord<>(
+                        SENSOR_TOPIC,
+                        request.getId(),
+                        request.toByteArray()
+                )
+        );
+
         responseObserver.onNext(Empty.getDefaultInstance());
         responseObserver.onCompleted();
     }
