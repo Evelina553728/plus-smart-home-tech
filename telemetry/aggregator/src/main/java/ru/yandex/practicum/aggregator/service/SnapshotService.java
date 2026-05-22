b9f5228984195126e2bd3ab9b82fd866b1b1b884
Evelina <evelinazolota55@gmail.com>
2026-05-22 23:27:51 +0300

    Спринт 20: обнова 4

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/SnapshotService.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/service/SnapshotService.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.aggregator.service;
 
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.kafka.telemetry.event.*;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +14,8 @@ public class SnapshotService {
     private final Map<String, SensorsSnapshotAvro> snapshots = new HashMap<>();
 
     public Optional<SensorsSnapshotAvro> updateState(SensorEventAvro event) {
-        SensorsSnapshotAvro snapshot = snapshots.computeIfAbsent(
+
+        SensorsSnapshotAvro currentSnapshot = snapshots.computeIfAbsent(
                 event.getHubId(),
                 hubId -> new SensorsSnapshotAvro(
                         hubId,
@@ -26,15 +29,46 @@ public class SnapshotService {
                 event.getPayload()
         );
 
-        SensorStateAvro oldState = snapshot.getSensorsState().get(event.getId());
+        SensorStateAvro oldState =
+                currentSnapshot.getSensorsState().get(event.getId());
 
-        if (oldState != null && oldState.getData().equals(newState.getData())) {
+        if (oldState != null &&
+                oldState.getData().equals(newState.getData())) {
+
             return Optional.empty();
         }
 
-        snapshot.getSensorsState().put(event.getId(), newState);
-        snapshot.setTimestamp(event.getTimestamp());
+        currentSnapshot.getSensorsState()
+                .put(event.getId(), newState);
 
-        return Optional.of(snapshot);
+        currentSnapshot.setTimestamp(event.getTimestamp());
+
+        SensorsSnapshotAvro snapshotCopy = copySnapshot(currentSnapshot);
+
+        return Optional.of(snapshotCopy);
+    }
+
+    private SensorsSnapshotAvro copySnapshot(SensorsSnapshotAvro snapshot) {
+
+        Map<String, SensorStateAvro> copiedStates = new HashMap<>();
+
+        for (Map.Entry<String, SensorStateAvro> entry :
+                snapshot.getSensorsState().entrySet()) {
+
+            SensorStateAvro state = entry.getValue();
+
+            SensorStateAvro copiedState = new SensorStateAvro(
+                    state.getTimestamp(),
+                    state.getData()
+            );
+
+            copiedStates.put(entry.getKey(), copiedState);
+        }
+
+        return new SensorsSnapshotAvro(
+                snapshot.getHubId(),
+                snapshot.getTimestamp(),
+                copiedStates
+        );
     }
 }
